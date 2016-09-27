7ab18c8a2db218f6573a1b3c1ba3aaf6b34c411c
Chris Hoffman <choffman@optoro.com>
2016-09-27 17:48:49 -0400

    moved command stub specs.
    they will get bigger soon and therefore shouldn't be in the main spec file

--- spec/shell_mock_spec.rb
+++ spec/shell_mock_spec.rb
@@ -1,31 +1,4 @@
 RSpec.describe ShellMock do
-  describe '::stub_command' do
-    before { ShellMock.enable }
-    after { ShellMock.disable }
-
-    let!(:stub) { ShellMock.stub_command('ls').and_return("\n") }
-    let!(:home_stub) { ShellMock.stub_command("ls $HOME").and_return("\n") }
-
-    it 'intercepts system' do
-      expect(system('ls')).to eq true
-
-      expect(stub.calls).to_not be_empty
-    end
-
-    it 'intercepts backtick' do
-      expect(`ls`).to eq "\n"
-
-      expect(stub.calls).to_not be_empty
-    end
-
-    it 'uses the "closest" stub' do
-      expect(`ls $HOME`).to eq "\n"
-
-      expect(home_stub.calls).to_not be_empty
-      expect(stub.calls).to be_empty
-    end
-  end
-
   describe '::dont_let_commands_run' do
     before do
       ShellMock.enable
