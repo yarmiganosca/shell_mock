55e66cc2761a370ffefc0ed500ad615eb0d9b689
Chris Hoffman <yarmiganosca@gmail.com>
2018-09-22 17:22:56 -0400

    reorganized the CallVerifier source
    I wanted to make it more immediately obvious that match_calls_when isn't just initializer sugar, that it's called several places.
    So I grouped all the methods that call it together.

--- lib/shell_mock/call_verifier.rb
+++ lib/shell_mock/call_verifier.rb
@@ -4,28 +4,6 @@ module ShellMock
       match_calls_when { |calls| calls.any? }
     end
 
-    def matches?(actual)
-      @actual = actual
-
-      condition.call(actual.calls)
-    end
-
-    def failure_message
-      "#{actual.command} was expected."
-    end
-
-    def failure_message_when_negated
-      "#{actual.command} was not expected."
-    end
-
-    def once
-      times(1)
-    end
-
-    def never
-      times(0)
-    end
-
     def times(n)
       match_calls_when { |calls| calls.size == n }
 
@@ -43,6 +21,28 @@ module ShellMock
       match_calls_when { |calls| calls.size > n }
 
       self
+    end
+
+    def once
+      times(1)
+    end
+
+    def never
+      times(0)
+    end
+
+    def matches?(actual)
+      @actual = actual
+
+      condition.call(actual.calls)
+    end
+
+    def failure_message
+      "#{actual.command} was expected."
+    end
+
+    def failure_message_when_negated
+      "#{actual.command} was not expected."
     end
 
     private
