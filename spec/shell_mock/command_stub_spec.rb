2d7e8a74dacd46ecbcc9ca8a48b8e7dff32049f4
Chris Hoffman <yarmiganosca@gmail.com>
2018-09-25 18:27:35 -0400

    add unit tests for CommandStub

--- spec/shell_mock/command_stub_spec.rb
+++ spec/shell_mock/command_stub_spec.rb
@@ -1,28 +1,69 @@
-require 'shell_mock/rspec'
-
 module ShellMock
-  RSpec.describe "::stub_commmand" do
-    before do
-      ShellMock.enable
-      ShellMock.let_commands_run
-    end
-    after { ShellMock.disable }
+  RSpec.describe CommandStub do
+    subject(:command_stub) { described_class.new('ls') }
 
-    let!(:stub)      { ShellMock.stub_command('ls') }
-    let!(:home_stub) { ShellMock.stub_command("ls $HOME") }
-
-    it 'uses the "closest" stub' do
-      `ls $HOME`
-
-      expect(home_stub.calls).to_not eq 0
-      expect(stub.calls).to eq 0
+    describe '#with_env' do
+      it 'sets env vars needed to match against this command stub' do
+        expect(command_stub.with_env({'X' => 'Y'}).env).to include({'X' => 'Y'})
+      end
     end
 
-    it 'but not too close' do
-      `ls /`
+    describe '#with_options' do
+      it 'sets options needed to match against this command stub' do
+        expect(command_stub.with_options({err: :out}).options).to include({err: :out})
+      end
+    end
 
-      expect(home_stub.calls).to eq 0
-      expect(stub.calls).to eq 0
+    describe '#and_output' do
+      it "specifies the output the invocation will generate instead of it's 'normal' output" do
+        expect(command_stub.and_output("42\n").output).to eq "42\n"
+      end
+    end
+
+    describe '#and_exit' do
+      it "specifies the status the invocation will exit with instead of it's 'normal' exit status" do
+        expect(command_stub.and_exit(42).exitstatus).to eq 42
+      end
+    end
+
+    describe '#and_return' do
+      it "specifies the output the invocation will generate instead of it's 'normal' output" do
+        expect(command_stub.and_return("42\n").output).to eq "42\n"
+      end
+
+      it "specifies that the invocation will exit with status 0" do
+        expect(command_stub.and_return("42\n").exitstatus).to eq 0
+      end
+    end
+
+    describe '#to_oneliner' do
+      context 'when output is set' do
+        before { command_stub.and_output("42\n") }
+
+        it 'echoes that output' do
+          expect(command_stub.to_oneliner).to include "echo '42\n'"
+        end
+      end
+
+      context 'when the exitstatus is set' do
+        before { command_stub.and_exit(42) }
+
+        it 'exits with that status' do
+          expect(command_stub.to_oneliner).to include "exit 42"
+        end
+      end
+
+      context 'when the output and exitstatus are set' do
+        before { command_stub.and_output("42\n").and_exit(42) }
+
+        it 'echoes that output' do
+          expect(command_stub.to_oneliner).to include "echo '42\n'"
+        end
+
+        it 'exits with that status' do
+          expect(command_stub.to_oneliner).to include "exit 42"
+        end
+      end
     end
   end
 end
--- spec/shell_mock_spec.rb
+++ spec/shell_mock_spec.rb
@@ -1,4 +1,29 @@
 RSpec.describe ShellMock do
+  describe "::stub_commmand" do
+    before do
+      ShellMock.enable
+      ShellMock.let_commands_run
+    end
+    after { ShellMock.disable }
+
+    let!(:stub)      { ShellMock.stub_command('ls') }
+    let!(:home_stub) { ShellMock.stub_command("ls $HOME") }
+
+    it 'uses the "closest" stub' do
+      `ls $HOME`
+
+      expect(home_stub.calls).to_not eq 0
+      expect(stub.calls).to eq 0
+    end
+
+    it 'but not too close' do
+      `ls /`
+
+      expect(home_stub.calls).to eq 0
+      expect(stub.calls).to eq 0
+    end
+  end
+
   describe '::dont_let_commands_run' do
     before do
       ShellMock.enable
