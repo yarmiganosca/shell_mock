caa179bb1efd4e4b47ee855de098d5795d5bc0a1
Chris Hoffman <yarmiganosca@gmail.com>
2018-09-26 13:58:21 -0400

    specs for matcher aliases

--- spec/shell_mock/rspec_spec.rb
+++ spec/shell_mock/rspec_spec.rb
@@ -9,36 +9,108 @@ RSpec.describe ShellMock do
 
   let!(:stub) { ShellMock.stub_command('ls').and_return("\n") }
 
-  context 'being called once' do
-    before { system('ls') }
+  describe '#have_been_called' do
+    context 'being called once' do
+      before { system('ls') }
 
-    it 'is matched when expected once' do
-      expect(stub).to have_been_called.once
+      it 'is matched when expected once' do
+        expect(stub).to have_been_called.once
+      end
+
+      it 'is not matched when not expected' do
+        expect(stub).to_not have_been_called.never
+      end
     end
 
-    it 'is not matched when not expected' do
-      expect(stub).to_not have_been_called.never
+    context 'being called twice' do
+      before { system('ls'); `ls` }
+
+      it 'is matched when expected twice' do
+        expect(stub).to have_been_called.times(2)
+      end
+
+      it 'is matched when expected more times than once' do
+        expect(stub).to have_been_called.more_than(1)
+      end
+
+      it 'is matched when expected fewer times than thrice' do
+        expect(stub).to have_been_called.fewer_than(3)
+        expect(stub).to have_been_called.less_than(3)
+      end
+
+      it 'is not matched against a negative expectation' do
+        expect(stub).to_not have_been_called.never
+      end
     end
   end
 
-  context 'being called twice' do
-    before { system('ls'); `ls` }
+  describe '#have_run' do
+    context 'being called once' do
+      before { system('ls') }
 
-    it 'is matched when expected twice' do
-      expect(stub).to have_been_called.times(2)
+      it 'is matched when expected once' do
+        expect(stub).to have_run.once
+      end
+
+      it 'is not matched when not expected' do
+        expect(stub).to_not have_run.never
+      end
     end
 
-    it 'is matched when expected more times than once' do
-      expect(stub).to have_been_called.more_than(1)
+    context 'being called twice' do
+      before { system('ls'); `ls` }
+
+      it 'is matched when expected twice' do
+        expect(stub).to have_run.times(2)
+      end
+
+      it 'is matched when expected more times than once' do
+        expect(stub).to have_run.more_than(1)
+      end
+
+      it 'is matched when expected fewer times than thrice' do
+        expect(stub).to have_run.fewer_than(3)
+        expect(stub).to have_run.less_than(3)
+      end
+
+      it 'is not matched against a negative expectation' do
+        expect(stub).to_not have_run.never
+      end
+    end
+  end
+
+  describe '#have_ran' do
+    context 'being called once' do
+      before { system('ls') }
+
+      it 'is matched when expected once' do
+        expect(stub).to have_ran.once
+      end
+
+      it 'is not matched when not expected' do
+        expect(stub).to_not have_ran.never
+      end
     end
 
-    it 'is matched when expected fewer times than thrice' do
-      expect(stub).to have_been_called.fewer_than(3)
-      expect(stub).to have_been_called.less_than(3)
-    end
+    context 'being called twice' do
+      before { system('ls'); `ls` }
 
-    it 'is not matched when not expected' do
-      expect(stub).to_not have_been_called.never
+      it 'is matched when expected twice' do
+        expect(stub).to have_ran.times(2)
+      end
+
+      it 'is matched when expected more times than once' do
+        expect(stub).to have_ran.more_than(1)
+      end
+
+      it 'is matched when expected fewer times than thrice' do
+        expect(stub).to have_ran.fewer_than(3)
+        expect(stub).to have_ran.less_than(3)
+      end
+
+      it 'is not matched against a negative expectation' do
+        expect(stub).to_not have_ran.never
+      end
     end
   end
 end
