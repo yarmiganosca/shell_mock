b80560c319fa50e6c251779c9fedd831b969185f
Chris Hoffman <choffman@optoro.com>
2019-11-27 22:01:16 -0500

    slight method name improvment

--- lib/shell_mock/core_ext/module.rb
+++ lib/shell_mock/core_ext/module.rb
@@ -3,14 +3,15 @@ module ShellMock
     module Module
       module Methods
         def deprecate(name, replacement, version)
-          old_name = :"#{name}_without_deprecation"
+          old_name = :"#{name}_without_deprecation_warning"
 
           alias_method old_name, name
 
           define_method(name) do |*args, &blk|
             warn "ShellMock: ##{name} is deprecated and will be removed by #{version}. "\
               "Please use #{replacement} instead."
-            send old_name, *args, &blk
+
+            send(old_name, *args, &blk)
           end
         end
       end
