b8bfeb732bd5708b52ae3134034aad4870748a5d
Chris Hoffman <choffman@optoro.com>
2019-11-27 21:51:02 -0500

    update Module extension to best practice
    Including our extensions in another module improves some debugging edge cases
    around who is the owner of a method object.

--- lib/shell_mock/core_ext/module.rb
+++ lib/shell_mock/core_ext/module.rb
@@ -1,17 +1,25 @@
-Module.class_exec do
-  def eigenclass
-    @eigenclass ||= class << self; self; end
-  end
+module ShellMock
+  module CoreExt
+    module Module
+      module Methods
+        def eigenclass
+          @eigenclass ||= class << self; self; end
+        end
 
-  def deprecate(name, replacement, version)
-    old_name = :"#{name}_without_deprecation"
+        def deprecate(name, replacement, version)
+          old_name = :"#{name}_without_deprecation"
 
-    alias_method old_name, name
+          alias_method old_name, name
 
-    define_method(name) do |*args, &blk|
-      warn "ShellMock: ##{name} is deprecated and will be removed by #{version}. "\
-        "Please use #{replacement} instead."
-      send old_name, *args, &blk
+          define_method(name) do |*args, &blk|
+            warn "ShellMock: ##{name} is deprecated and will be removed by #{version}. "\
+              "Please use #{replacement} instead."
+            send old_name, *args, &blk
+          end
+        end
+      end
     end
   end
 end
+
+Module.include ShellMock::CoreExt::Module::Methods
